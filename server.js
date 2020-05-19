75182e0964fc22bfb742c3dc481ac75a3c8aac69
Jaime Sanders <jaimelyn517@gmail.com>
2020-05-19 14:15:10 -0700

    loaded intial database

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 require('./lib/client').connect();
 
+const client = require('./lib/client');
+
 const app = require('./lib/app');
 
 const PORT = process.env.PORT || 7890;
@@ -9,3 +11,5 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
 });
+
+app.get('/favorites', async(req, res) => { const data = await client.query('SELECT * from favorites'); res.json(data.rows);});
